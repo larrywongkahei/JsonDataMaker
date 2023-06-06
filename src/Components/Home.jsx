0a971a55f67482db01fcae2d5d5dc51d9a895db0
Maryhei <makemak123@gmail.com>
2023-06-06 17:30:07 +0100

    Added three code tag to show different programming language to fetch data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,69 @@ import React from "react";
 
 export function Home(){
     return (
-        <h1>
-            This is Home page.
-        </h1>
+        <div>
+            <h1>
+                There are three categories for now
+            </h1>
+            <h4>
+                tutorial
+            </h4>
+            <table style={{width:"100%"}}>
+                <tr>
+                    <td style={{textAlign:"center", width:"30%", borderRight:"1px solid white"}}>
+                        C#
+                    </td>
+                    <td>
+                        <pre style={{width:"670px", backgroundColor:"grey", height:"180px", paddingLeft:"10px", paddingTop:"10px", margin:"auto", overflow:"scroll"}}>
+                            <code style={{backgroundColor:"grey"}}>
+                                using (HttpClient client = new HttpClient())&#123;<br /><br />
+                                &nbsp;using (HttpResponseMessage res = await client.GetAsync(baseURL))&#123;<br /><br />
+                                &nbsp;&nbsp;using (HttpContent content = res.Content)&#123;<br /><br />
+                                &nbsp;&nbsp;&nbsp;string data = await content.ReadAsStringAsync();<br /><br />
+                                &nbsp;&nbsp;&nbsp;#Do whatever you want with the jsondata<br /><br />
+                                &nbsp;&nbsp;&#125;<br /><br />
+                                &nbsp;&#125;<br /><br />
+                                &#125;
+                            </code>
+                        </pre>
+                    </td>
+                </tr>
+                </table>
+                <table style={{width:"100%"}}>
+                <tr>
+                    <td style={{textAlign:"center", width:"30%", borderRight:"1px solid white"}}>
+                        Javascript
+                    </td>
+                    <td>
+                        <pre style={{width:"670px", backgroundColor:"grey", height:"70px", paddingLeft:"10px", paddingTop:"10px", margin:"auto"}}>
+                            <code style={{backgroundColor:"grey"}}>
+                                fetch("The endpoint here")<br/>
+                                .then(res => res.json())<br/>
+                                .then(data => #Do whatever you want with the jsondata)
+                            </code>
+                        </pre>
+                    </td>
+                </tr>
+                </table>
+                <table style={{width:"100%"}}>
+                <tr>
+                    <td style={{textAlign:"center", width:"30%", borderRight:"1px solid white"}}>
+                        Python
+                    </td>
+                    <td>
+                        <pre style={{width:"670px", backgroundColor:"grey", height:"120px", paddingLeft:"10px", paddingTop:"10px", margin:"auto"}}>
+                            <code style={{backgroundColor:"grey"}}>
+                            import requests<br /><br />
+                            request = requests.get("The endpoint here")<br />
+                            data = request.text<br />
+                            #Do whatever you want with the jsondata
+                            </code>
+                        </pre>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+
     )
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export function NarBar(){
     return (
-        <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", paddingLeft:"10px"}}>
-            <Link to={"/"} style={{fontSize:"42px", fontWeight:"bold", textDecoration:"none", color:"black"}}>
+        <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", paddingLeft:"10px", color:"white"}}>
+            <Link to={"/"} style={{fontSize:"42px", fontWeight:"bold", textDecoration:"none", color:"white"}}>
                 Here is the topic
             </Link>
             <div style={{paddingRight:"10px"}}>
                 <a>
-                    <Link to={"/"} style={{textDecoration:"none", color:"black", paddingRight:"10px"}}>Home</Link>
+                    <Link to={"/"} style={{textDecoration:"none", paddingRight:"10px", color:"white"}}>Home</Link>
                 </a>
                 <a>
-                    <Link to={"/customjson"} style={{textDecoration:"none", color:"black", paddingRight:"10px"}}>Create</Link>
+                    <Link to={"/customjson"} style={{textDecoration:"none", paddingRight:"10px", color:"white"}}>Create</Link>
                 </a>
                 <a>
-                    <Link to={"/contact"} style={{textDecoration:"none", color:"black"}}>Contact</Link>
+                    <Link to={"/contact"} style={{textDecoration:"none", color:"white"}}>Contact</Link>
                 </a>
             </div>
         </div>
