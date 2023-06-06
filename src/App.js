9b2f8faf294704948ac80d80a1d1179ed6dc3084
Maryhei <makemak123@gmail.com>
2023-06-06 14:31:03 +0100

    Added navbar components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Contact } from "./Components/Contact"
 import { Home } from './Components/Home';
+import { NarBar } from './Components/NavBar';
 
 function App() {
   return (
     <Router className="App">
+      <NarBar />
      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />}/>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function NarBar(){
+    return (
+        <h1>I am narbar</h1>
+    )
+}
